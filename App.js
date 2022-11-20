6dbe2f10d65373615cce2988edae135037cdec0a
ACHI9944 <achi9944@gmail.com>
2022-11-20 17:36:25 +0400

    Starting project with basics. Added screens, installed camera package

---
+++ App.js
@@ -0,0 +1,47 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
+import AddPlace from "./screen/AddPlace";
+import AllPlaces from "./screen/AllPlaces";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your Favorite Places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  color={tintColor}
+                  size={24}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{ title: "Add a new Place" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
