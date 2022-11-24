b91c3fa4f778a14be8f2771f25b7161fc17db6d5
ACHI9944 <achi9944@gmail.com>
2022-11-24 17:16:35 +0400

    Styled place items

--- screen/AllPlaces.js
+++ screen/AllPlaces.js
@@ -5,11 +5,25 @@ import PlacesList from "../components/Places/PlacesList";
 function AllPlaces({ route }) {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   const isFocused = useIsFocused();
+  // useEffect(() => {
+  //   if (isFocused && route.params) {
+  //     setLoadedPlaces((curPlaces) => {
+  //       const existingPlace = curPlaces.find(
+  //         (existPlace) => existPlace.id === route.params.place.id
+  //       );
+  //       if (!!existingPlace) {
+  //         return [...curPlaces];
+  //       } else {
+  //         return [...curPlaces, route.params.place];
+  //       }
+  //     });
+  //   }
+  // }, [isFocused , route]);
   useEffect(() => {
-    if (isFocused && route.params) {
+    if (route.params) {
       setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
     }
-  }, [isFocused, route]);
+  }, [route]);
   return <PlacesList places={loadedPlaces} />;
 }
 
